fbd207067c1cc98962dfcb2b8b63531f9e3da4b2
Abdelrahman Elogeel <abdelrahman.elogeel@microsoft.com>
2012-02-22 15:49:44 -0800

    Update checkstyle xml

--- tests/autoload.php
+++ tests/autoload.php
@@ -6,14 +6,14 @@ spl_autoload_register(
       static $classes = null;
       if ($classes === null) {
          $classes = array(
-            'azureauthenticationtest' => '/Unit/WindowsAzure/Services/Core/Authentication/AzureAuthenticationTest.php',
-            'blobqueuesharedkeytest' => '/Unit/WindowsAzure/Services/Core/Authentication/BlobQueueSharedKeyTest.php',
-            'configurationtest' => '/Unit/WindowsAzure/Services/Core/ConfigurationTest.php',
-            'pear2\\tests\\mock\\windowsazure\\core\\httpclientmock' => '/Mock/WindowsAzure/Core/HttpClientMock.php',
-            'pear2\\tests\\mock\\windowsazure\\services\\core\\authentication\\azureauthenticationmock' => '/Mock/WindowsAzure/Services/Core/Authentication/AzureAuthenticationMock.php',
-            'pear2\\tests\\mock\\windowsazure\\services\\core\\authentication\\blobqueuesharedkeymock' => '/Mock/WindowsAzure/Services/Core/Authentication/BlobQueueSharedKeyMock.php',
-            'pear2\\tests\\unit\\testresources' => '/Unit/TestResources.php',
-            'queuerestproxytest' => '/Unit/WindowsAzure/Services/Queue/QueueRestProxyTest.php'
+            'azureauthenticationtest' => '/unit/WindowsAzure/Services/Core/Authentication/AzureAuthenticationTest.php',
+            'blobqueuesharedkeytest' => '/unit/WindowsAzure/Services/Core/Authentication/BlobQueueSharedKeyTest.php',
+            'configurationtest' => '/unit/WindowsAzure/Services/Core/ConfigurationTest.php',
+            'pear2\\tests\\mock\\windowsazure\\core\\httpclientmock' => '/mock/WindowsAzure/Core/HttpClientMock.php',
+            'pear2\\tests\\mock\\windowsazure\\services\\core\\authentication\\azureauthenticationmock' => '/mock/WindowsAzure/Services/Core/Authentication/AzureAuthenticationMock.php',
+            'pear2\\tests\\mock\\windowsazure\\services\\core\\authentication\\blobqueuesharedkeymock' => '/mock/WindowsAzure/Services/Core/Authentication/BlobQueueSharedKeyMock.php',
+            'pear2\\tests\\unit\\testresources' => '/unit/TestResources.php',
+            'queuerestproxytest' => '/unit/WindowsAzure/Services/Queue/QueueRestProxyTest.php'
           );
       }
       $cn = strtolower($class);
