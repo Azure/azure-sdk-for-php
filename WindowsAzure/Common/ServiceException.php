7cba4f07760b2af3f492096eaaeb7779ccaf9a1a
Jeff Wilcox <jeffwilcox@gmail.com>
2012-05-23 10:37:46 -0700

    Replacing author header entries
    Replacing the author headers to be the team contact information for
    consistency.

--- WindowsAzure/Common/ServiceException.php
+++ WindowsAzure/Common/ServiceException.php
@@ -16,7 +16,7 @@
  *
  * @category  Microsoft
  * @package   WindowsAzure\Common
- * @author    Abdelrahman Elogeel <Abdelrahman.Elogeel@microsoft.com>
+ * @author    Azure PHP SDK <azurephpsdk@microsoft.com>
  * @copyright 2012 Microsoft Corporation
  * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
  * @link      http://pear.php.net/package/azure-sdk-for-php
@@ -30,7 +30,7 @@ use WindowsAzure\Common\Internal\Resources;
  *
  * @category  Microsoft
  * @package   WindowsAzure\Common
- * @author    Abdelrahman Elogeel <Abdelrahman.Elogeel@microsoft.com>
+ * @author    Azure PHP SDK <azurephpsdk@microsoft.com>
  * @copyright 2012 Microsoft Corporation
  * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
  * @version   Release: @package_version@
