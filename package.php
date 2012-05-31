049e14db8136495d200b898b6024da86f50a64b8
Abdelrahman Elogeel <Abdelrahman.Elogeel@microsoft.com>
2012-05-31 11:50:16 -0700

    Apply code review

--- package.php
+++ package.php
@@ -23,21 +23,36 @@
 require_once 'PEAR/PackageFileManager2.php';
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
-$package = 'WindowsAzure';
-$channel = 'pear.windowsazure.com';
-$summary = '
-Summary for Windows Azure SDK for PHP
+$package 	          = 'WindowsAzure';
+$channel 	          = 'pear.blob.core.windows.net';
+$release_version      = '0.1.0';
+$api_version          = '0.1.0';
+$release_state        = 'beta';
+$api_state            = 'beta';
+$min_php_version      = '5.3.0';
+$min_pear_version     = '1.8.0';
+$license              = 'Apache 2.0';
+$license_agreement    = 'http://www.apache.org/licenses/LICENSE-2.0';
+$summary              = '
+Official PHP SDK for Microsoft Windows Azure.
 ';
-$description = '
-Description for Windows Azure SDK for PHP
+$description          = '
+The Windows Azure client libraries provide easy access to all Windows Azure REST services 
+including Tables, Blobs, Queues, Service Bus, Service Management and Service Runtime.
 ';
-$release_version = '1.0.0';
-$release_state = 'stable';
-$release_notes = '
-* Full support of Windows Azure sotrage services Queue, Blob and Table.
-* Full support of Windows Azure service bus.
+$release_notes        = '
+* Full support of Queue, Blob and Table services.
+* Full support of Service Bus.
+* Full support of Service Runtime.
 * Support for storage services and affinity groups management operations.
 ';
+$default_pear_channel = 'pear.php.net';
+$dependencies         = array(
+    array('required', 'HTTP_Request2', 	          $default_pear_channel),
+    array('required', 'Mail_Mime',                $default_pear_channel),
+    array('required', 'Mail_mimeDecode',          $default_pear_channel),
+    array('optional', 'PEAR_PackageFileManager2', $default_pear_channel),
+);
 
 $p = &PEAR_PackageFileManager2::importOptions(
     dirname(__FILE__) . DIRECTORY_SEPARATOR . 'package.xml',
@@ -62,19 +77,18 @@ $p->setPackageType('php');
 $p->addRelease();
 $p->clearDeps();
 $p->setChannel($channel);
-$p->setLicense('Apache 2.0', 'https://github.com/WindowsAzure/azure-sdk-for-php/blob/master/LICENSE.txt');
+$p->setLicense($license, $license_agreement);
 $p->setReleaseVersion($release_version);
-$p->setAPIVersion('1.0.0');
+$p->setAPIVersion($api_version);
 $p->setReleaseStability($release_state);
-$p->setAPIStability('stable');
-$p->setPhpDep('5.3.0');
+$p->setAPIStability($api_state);
+$p->setPhpDep($min_php_version);
 // If this run twice the lead entry will be duplicated. Make sure that it just run once.
 //$p->addMaintainer('lead', 'AzurePHPSDK', 'Azure PHP SDK', 'azurephpsdk@microsoft.com');
-$p->setPearinstallerDep('1.8.0');
-$p->addPackageDepWithChannel('required', 'HTTP_Request2', 'pear.php.net');
-$p->addPackageDepWithChannel('required', 'Mail_Mime', 'pear.php.net');
-$p->addPackageDepWithChannel('required', 'Mail_mimeDecode', 'pear.php.net');
-$p->addPackageDepWithChannel('optional', 'PEAR_PackageFileManager2', 'pear.php.net');
+$p->setPearinstallerDep($min_pear_version);
+foreach($dependencies as $dependency) {
+    $p->addPackageDepWithChannel($dependency[0], $dependency[1], $dependency[2]);
+}
 // The replacement doesn't work check: https://github.com/WindowsAzure/azure-sdk-for-php/issues/413
 $p->addReplacement('WindowsAzure/*.php', 'package-info', '@package_version@', 'version');
 $p->generateContents();
