7ada713ea0a9dbcf44fa1d9524de00efa5068dcd
ogail <zikas_zak@hotmail.com>
2012-07-05 15:53:03 -0700

    Fix #541

--- WindowsAzure/Table/Models/EdmType.php
+++ WindowsAzure/Table/Models/EdmType.php
@@ -137,7 +137,7 @@ class EdmType
         case null:
             // NULL also is treated as EdmType::STRING
             // Convert special characters to HTML entities.
-            return htmlspecialchars($value);
+            return $value;
             
         case EdmType::BINARY:
             return base64_encode($value);
