7f33110ddb906ac00b3e21e6b74317695b7d8448
ogail <zikas_zak@hotmail.com>
2012-07-25 10:39:12 -0700

    Fix CheckStyle errors

--- WindowsAzure/Blob/Models/BlobBlockType.php
+++ WindowsAzure/Blob/Models/BlobBlockType.php
@@ -54,10 +54,10 @@ class BlobBlockType
         case self::COMMITTED_TYPE:
         case self::LATEST_TYPE:
         case self::UNCOMMITTED_TYPE:
-            return true;
+        return true;
         
         default:
-            return false;
+        return false;
         }
     }
 }
