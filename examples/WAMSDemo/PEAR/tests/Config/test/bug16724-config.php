2aca94f1cf9ce3cfe989c191ed246ca0cae2b789
Vladimir Tsisnatiy <v-vltsis@microsoft.com>
2014-01-23 00:19:15 -0800

    Delete old demo code

--- examples/WAMSDemo/PEAR/tests/Config/test/bug16724-config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('MY_CONFIG_OPTION', 'foo');
-define('HOST12_3', 'foo');
-?>
--- examples/WAMSDemo/PEAR/tests/MIME_Type/tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-//PEAR is not E_STRICT compatible
-error_reporting(error_reporting() & ~E_STRICT);
-
-chdir(dirname(__FILE__) . '/../');
-?>
