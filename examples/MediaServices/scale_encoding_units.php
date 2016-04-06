68396579eed6368aefb50b2dd2c16055a83b1195
Yaqi Yang <yaqi.yang@microsoft.com>
2016-04-06 14:01:46 -0700

    Fix examples to work with the current SDK code

--- examples/MediaServices/scale_encoding_units.php
+++ examples/MediaServices/scale_encoding_units.php
@@ -1,11 +1,36 @@
 <?php
+
+/**
+ * LICENSE: Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * PHP version 5
+ *
+ * @category  Microsoft
+ * @package   Client
+ * @author    Azure PHP SDK <azurephpsdk@microsoft.com>
+ * @copyright 2012 Microsoft Corporation
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @link      https://github.com/windowsazure/azure-sdk-for-php
+ */
+
 require_once 'vendor/autoload.php';
-require_once "WindowsAzure/WindowsAzure.php";
 
 use WindowsAzure\Common\ServicesBuilder;
 use WindowsAzure\Common\Internal\MediaServicesSettings;
 use WindowsAzure\MediaServices\Models\EncodingReservedUnit;
 use WindowsAzure\MediaServices\Models\EncodingReservedUnitType;
+
+// Settings
+date_default_timezone_set('America/Los_Angeles');
 
 $account = "<your media services account name>";
 $secret = "<your media services account key>";
