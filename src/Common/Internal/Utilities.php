a85cf6d69bebfb7faf5c1b9dfd23e8d06ee91efa
Mike Kasberg <kasberg.mike@gmail.com>
2018-10-04 20:49:28 -0600

    Fix getGuid() on max mt_rand
    As identified by @znrk (#984), getGuid() is broken.
    
    In fact, it's not only broken on the line identified for clock_seq_low,
    but also in 2 other places. mt_rand() is inclusive for both the max and
    the min, so mt_rand(0, 256) produces a max value of 0x100 rather than
    0xff, overflowing from 2 bits to 3 bits. The fix is simple - changing
    the max value on the 3 lines where it's broken to ensure that the
    generated GUID is in line with the spec for version 4 (random GUID) at
    <https://tools.ietf.org/html/rfc4122>.
    
    This commit fixes #984.
    
    I'll follow this commit up with a commit that introduces tests to
    illustrate and cover this scenario.

--- src/Common/Internal/Utilities.php
+++ src/Common/Internal/Utilities.php
@@ -548,13 +548,13 @@ class Utilities
             mt_rand(0, 65535),
             mt_rand(0, 65535),          // 32 bits for "time_low"
             mt_rand(0, 65535),          // 16 bits for "time_mid"
-            mt_rand(0, 4096) + 16384,   // 16 bits for "time_hi_and_version", with
+            mt_rand(0, 4095) + 16384,   // 16 bits for "time_hi_and_version", with
                                         // the most significant 4 bits being 0100
                                         // to indicate randomly generated version
-            mt_rand(0, 64) + 128,       // 8 bits  for "clock_seq_hi", with
+            mt_rand(0, 63) + 128,       // 8 bits  for "clock_seq_hi", with
                                         // the most significant 2 bits being 10,
                                         // required by version 4 GUIDs.
-            mt_rand(0, 256),            // 8 bits  for "clock_seq_low"
+            mt_rand(0, 255),            // 8 bits  for "clock_seq_low"
             mt_rand(0, 65535),          // 16 bits for "node 0" and "node 1"
             mt_rand(0, 65535),          // 16 bits for "node 2" and "node 3"
             mt_rand(0, 65535)           // 16 bits for "node 4" and "node 5"
